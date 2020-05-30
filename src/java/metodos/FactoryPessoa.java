ebfb5571b65d1222bef63771c526610708cdb73c
Arunia e Silinto <66164677+ninanani424@users.noreply.github.com>
2020-05-30 17:37:47 +0530

    1.1

---
+++ src/java/metodos/FactoryPessoa.java
@@ -0,0 +1,253 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos;
+
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Pessoa;
+
+public class FactoryPessoa extends Factory{
+    
+    @Override
+    public  Object newElement() {
+        return new Pessoa();
+    }
+
+    @Override
+    public  Object read(Object primaryKey){
+       Pessoa c;
+       // Criar um Pessoa
+       c = (Pessoa) this.newElement();
+       String query = "SELECT * FROM pessoa WHERE idessoa = ?";
+      try {
+        Connection conn = Conexao.getInstance();
+        PreparedStatement pt = conn.prepareStatement(query);
+        pt.setString(1,(String) primaryKey);
+        ResultSet rs = pt.executeQuery();
+        if (rs.next()){
+                //Inizializar um Pessoa
+                c.setIdPessoa(rs.getInt("idPessoa"));
+                c.setNome(rs.getString("nome"));
+                c.setApelido(rs.getString("apelido"));
+                c.setSexo(rs.getString("sexo"));
+                c.setDataNascimento(rs.getString("dataNascimento"));
+                c.setProvinciaNascimento(rs.getString("provinciaNascimento"));
+                c.setCidadeNascimento(rs.getString("cidadeNascimento"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setTelefone(rs.getString("telefone"));
+             }
+             pt.close();
+             conn.close();
+        }catch(Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,ex.toString(),ex);
+        }
+       return c;
+    }
+
+    @Override
+    public  int delete(Object primaryKey){
+       int ris = 0;
+       String query = "DELETE FROM pessoa WHERE idPessoa = ?";
+       try {
+            Connection conn = Conexao.getInstance();
+            PreparedStatement pt = conn.prepareStatement(query);
+            pt.setString(1,(String) primaryKey);
+            ris = pt.executeUpdate();
+            pt.close();
+            conn.close();
+        }catch(Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,ex.toString(),ex);
+        }
+       return ris;
+    }
+
+    @Override
+    public int update(Object o){
+        int ris = 0;
+        try {
+            Connection conn = Conexao.getInstance();
+            String sqlInsert = "UPDATE pessoa SET"
+                    + " idPessoa =?, nome = ?, apelido = ?,"
+                    + " sexo = ?, dataNascimento = ?, provinciaNascimento = ?,"
+                    + " cidadeNascimento = ?, endereco = ?, telefone = ?"
+                    + " WHERE idPessoa = ?";
+            PreparedStatement pt = conn.prepareStatement(sqlInsert);
+            pt.setInt(1,((Pessoa) o).getIdPessoa());
+            pt.setString(2,((Pessoa) o).getNome());
+            pt.setString(3,((Pessoa) o).getApelido());
+            pt.setString(2,((Pessoa) o).getSexo());
+            pt.setString(4,((Pessoa) o).getDataNascimento());
+            pt.setString(5,((Pessoa) o).getProvinciaNascimento());
+            pt.setString(6,((Pessoa) o).getCidadeNascimento());
+            pt.setString(7,((Pessoa) o).getEndereco());
+            pt.setString(8,((Pessoa) o).getTelefone());
+            pt.setInt(9,((Pessoa) o).getIdPessoa());
+            
+            ris = pt.executeUpdate();
+            pt.close();
+            conn.close();
+        } catch (Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,ex.toString(),ex);
+        }
+
+        //Se insert OK return object se nao null
+        return ris;
+    }
+
+
+    @Override
+    public Object insert(Object o){
+        boolean ris = false;
+        try {
+            Connection conn = Conexao.getInstance();
+            String sqlInsert = "INSERT INTO pessoa VALUES(null,?,?,?,?,?,?,?,?)";
+            PreparedStatement pt = conn.prepareStatement(sqlInsert);
+           // pt.setString(1,((Pessoa) o).getIdpessoa());
+            pt.setString(1,((Pessoa) o).getNome());
+            pt.setString(2,((Pessoa) o).getApelido());
+            pt.setString(3,((Pessoa) o).getSexo());
+            pt.setString(4,((Pessoa) o).getDataNascimento());
+            pt.setString(5,((Pessoa) o).getProvinciaNascimento());
+            pt.setString(6,((Pessoa) o).getCidadeNascimento());
+            pt.setString(7,((Pessoa) o).getEndereco());
+            pt.setString(8,((Pessoa) o).getTelefone());
+
+            ris = pt.execute();
+            pt.close();
+            conn.close();
+
+            ris = pt.execute();
+            pt.close();
+            conn.close();
+        } catch (Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,ex.toString(),ex);
+        }
+
+        //Se insert OK return object se nao null
+        if (ris) {
+        return o;
+        }
+        return null;
+    }
+
+    @Override
+    public java.util.ArrayList<Object> selectAll() {
+        String query = "SELECT * FROM pessoa";
+        Pessoa c;
+        ArrayList pessoas = new ArrayList<Pessoa>();
+        try {
+             Connection conn = Conexao.getInstance();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(query);
+             while(rs.next()){
+                // Criar um Pessoa
+                c = (Pessoa) this.newElement();
+
+                //Inizializar um Pessoa
+                c.setIdPessoa(rs.getInt("idPessoa"));
+                c.setNome(rs.getString("nome"));
+                c.setApelido(rs.getString("apelido"));
+                c.setSexo(rs.getString("sexo"));
+                c.setDataNascimento(rs.getString("dataNascimento"));
+                c.setProvinciaNascimento(rs.getString("provinciaNascimento"));
+                c.setCidadeNascimento(rs.getString("cidadeNascimento"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setTelefone(rs.getString("telefone"));
+
+                //juntar um Pessoa
+                pessoas.add(c);
+             }
+             st.close();
+             conn.close();
+        }catch(Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,ex.toString(),ex);
+        }
+        return pessoas;
+    }
+
+
+     public java.util.ArrayList<Object> selectPessoasByNome(String nome) {
+        String query = "SELECT * FROM pessoa WHERE nome like ? ";
+        Pessoa c;
+        ArrayList pessoas = new ArrayList<Pessoa>();
+        try {
+             Connection conn = Conexao.getInstance();
+             PreparedStatement ps = conn.prepareStatement(query);
+             ps.setString(1, "%"+nome+"%");
+             ResultSet rs = ps.executeQuery();
+             while(rs.next()){
+                // Criar um Pessoa
+                c = (Pessoa) this.newElement();
+
+                //Inizializar um Pessoa
+                c.setIdPessoa(rs.getInt("idPessoa"));
+                c.setNome(rs.getString("nome"));
+                c.setApelido(rs.getString("apelido"));
+                c.setSexo(rs.getString("sexo"));
+                c.setDataNascimento(rs.getString("dataNascimento"));
+                c.setProvinciaNascimento(rs.getString("provinciaNascimento"));
+                c.setCidadeNascimento(rs.getString("cidadeNascimento"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setTelefone(rs.getString("telefone"));
+
+                //juntar um Pessoa
+                pessoas.add(c);
+             }
+             ps.close();
+             conn.close();
+        }catch(Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,ex.toString(),ex);
+        }
+        return pessoas;
+
+    }
+
+
+
+public java.util.ArrayList<Object> selectPessoasByPrimaryKey(String primaryKey) {
+        String query = "SELECT * FROM pessoa WHERE idPessoa = ? ";
+        Pessoa c;
+        ArrayList pessoas = new ArrayList<Pessoa>();
+        try {
+             Connection conn = Conexao.getInstance();
+             PreparedStatement ps = conn.prepareStatement(query);
+             ps.setString(1, primaryKey);
+             ResultSet rs = ps.executeQuery();
+             while(rs.next()){
+                // Criar um produto
+                c = (Pessoa) this.newElement();
+
+                //Inizializar um pessoao
+                c.setIdPessoa(rs.getInt("idPessoa"));
+                c.setNome(rs.getString("nome"));
+                c.setApelido(rs.getString("apelido"));
+                c.setSexo(rs.getString("sexo"));
+                c.setDataNascimento(rs.getString("dataNascimento"));
+                c.setProvinciaNascimento(rs.getString("provinciaNascimento"));
+                c.setCidadeNascimento(rs.getString("cidadeNascimento"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setTelefone(rs.getString("telefone"));
+
+                //juntar um pessoa
+                pessoas.add(c);
+             }
+             ps.close();
+             conn.close();
+        }catch(Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE,ex.toString(),ex);
+        }
+        return pessoas;
+
+}
+
+    
+}
---
+++ src/java/modelo/Pessoa.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author mulessiua
+ */
+@Entity
+@Table(name = "Pessoa")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Pessoa.findAll", query = "SELECT p FROM Pessoa p")
+    , @NamedQuery(name = "Pessoa.findByIdPessoa", query = "SELECT p FROM Pessoa p WHERE p.idPessoa = :idPessoa")
+    , @NamedQuery(name = "Pessoa.findByNome", query = "SELECT p FROM Pessoa p WHERE p.nome = :nome")
+    , @NamedQuery(name = "Pessoa.findByApelido", query = "SELECT p FROM Pessoa p WHERE p.apelido = :apelido")
+    , @NamedQuery(name = "Pessoa.findBySexo", query = "SELECT p FROM Pessoa p WHERE p.sexo = :sexo")
+    , @NamedQuery(name = "Pessoa.findByDataNascimento", query = "SELECT p FROM Pessoa p WHERE p.dataNascimento = :dataNascimento")
+    , @NamedQuery(name = "Pessoa.findByProvinciaNascimento", query = "SELECT p FROM Pessoa p WHERE p.provinciaNascimento = :provinciaNascimento")
+    , @NamedQuery(name = "Pessoa.findByCidadeNascimento", query = "SELECT p FROM Pessoa p WHERE p.cidadeNascimento = :cidadeNascimento")
+    , @NamedQuery(name = "Pessoa.findByEndereco", query = "SELECT p FROM Pessoa p WHERE p.endereco = :endereco")
+    , @NamedQuery(name = "Pessoa.findByTelefone", query = "SELECT p FROM Pessoa p WHERE p.telefone = :telefone")})
+public class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idPessoa")
+    private Integer idPessoa;
+    @Size(max = 45)
+    @Column(name = "nome")
+    private String nome;
+    @Size(max = 45)
+    @Column(name = "apelido")
+    private String apelido;
+    @Size(max = 45)
+    @Column(name = "sexo")
+    private String sexo;
+    @Size(max = 45)
+    @Column(name = "dataNascimento")
+    private String dataNascimento;
+    @Size(max = 45)
+    @Column(name = "provinciaNascimento")
+    private String provinciaNascimento;
+    @Size(max = 45)
+    @Column(name = "cidadeNascimento")
+    private String cidadeNascimento;
+    @Size(max = 45)
+    @Column(name = "endereco")
+    private String endereco;
+    @Size(max = 45)
+    @Column(name = "telefone")
+    private String telefone;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public Integer getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Integer idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getProvinciaNascimento() {
+        return provinciaNascimento;
+    }
+
+    public void setProvinciaNascimento(String provinciaNascimento) {
+        this.provinciaNascimento = provinciaNascimento;
+    }
+
+    public String getCidadeNascimento() {
+        return cidadeNascimento;
+    }
+
+    public void setCidadeNascimento(String cidadeNascimento) {
+        this.cidadeNascimento = cidadeNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPessoa != null ? idPessoa.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pessoa)) {
+            return false;
+        }
+        Pessoa other = (Pessoa) object;
+        if ((this.idPessoa == null && other.idPessoa != null) || (this.idPessoa != null && !this.idPessoa.equals(other.idPessoa))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Pessoa[ idPessoa=" + idPessoa + " ]";
+    }
+    
+}
